37d32402881c3c73248e4dff79feb8f8f1ec1ad5
伊藤吉伸 <fiv.e2b3s@gmail.com>
2018-08-04 23:41:16 +0900

    first commit

---
+++ src/test/java/com/sample/docs/spring/springrestdocssample/SpringRestDocsSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.docs.spring.springrestdocssample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestDocsSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
